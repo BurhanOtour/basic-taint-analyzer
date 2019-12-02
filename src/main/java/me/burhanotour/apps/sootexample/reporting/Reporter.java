a475e508e3c794902d418babe01f1b5f0173f498
Otour, Burhan <burhan.a.otour@hotmail.com>
2019-12-02 01:52:46 +0100

    initial commit

---
+++ src/main/java/me/burhanotour/apps/sootexample/reporting/Reporter.java
@@ -0,0 +1,14 @@
+package me.burhanotour.apps.sootexample.reporting;
+
+
+import soot.SootMethod;
+import soot.Unit;
+
+public class Reporter {
+
+	public void report(SootMethod method, Unit source, Unit sink) {
+		System.out.println("Found a flow in method " + method);
+		System.out.println("\tSource: " + source);
+		System.out.println("\tSink: " + sink);
+	}
+}
